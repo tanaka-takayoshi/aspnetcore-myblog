5d0938a714e0cd585d0e85d2321cacbf08d8c357
tanaka_733 <tanaka-takayoshi@users.noreply.github.com>
2017-09-17 13:04:08 +0900

    Changed db path.

--- BlogTemplate/Startup.cs
+++ BlogTemplate/Startup.cs
@@ -24,7 +24,7 @@ namespace My_Blog
         {
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite("Data Source=blogging.db"));
+                options.UseSqlite("Data Source=data/blogging.db"));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
